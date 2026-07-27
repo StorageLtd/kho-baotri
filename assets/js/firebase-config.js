7f293fd42a5ed4f1bf5a7d78b10ba39fdd392400
LTDSTORAGE <thainguyentk911@gmail.com>
2026-07-27 14:14:41 +0700

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,12 +1,6 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+// Firebase Web App configuration for LTD CMMS.
+// Authentication is initialized in auth.js using Firebase's browser CDN modules.
+export const firebaseConfig = {
   apiKey: "AIzaSyAvpguQWSOCpNiCFej27hwK5ulZSPSBRk4",
   authDomain: "ltdst-80b3e.firebaseapp.com",
   databaseURL: "https://ltdst-80b3e-default-rtdb.asia-southeast1.firebasedatabase.app",
@@ -16,7 +10,3 @@ const firebaseConfig = {
   appId: "1:605858752960:web:e28b803b2b7f77b9824025",
   measurementId: "G-BT2RV4VVR8"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
