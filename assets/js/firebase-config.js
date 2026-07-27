cb60101741b0930d5874388f57829aa7c3dfc7b7
LTDSTORAGE <thainguyentk911@gmail.com>
2026-07-27 14:07:51 +0700

    ket noi

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,11 +1,22 @@
-// Paste the Web app configuration from Firebase Console > Project settings here.
-// This file intentionally contains no credentials or staff accounts.
-export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_FIREBASE_API_KEY",
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAvpguQWSOCpNiCFej27hwK5ulZSPSBRk4",
   authDomain: "ltdst-80b3e.firebaseapp.com",
   databaseURL: "https://ltdst-80b3e-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "ltdst-80b3e",
   storageBucket: "ltdst-80b3e.firebasestorage.app",
-  messagingSenderId: "REPLACE_WITH_SENDER_ID",
-  appId: "REPLACE_WITH_APP_ID"
+  messagingSenderId: "605858752960",
+  appId: "1:605858752960:web:e28b803b2b7f77b9824025",
+  measurementId: "G-BT2RV4VVR8"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
